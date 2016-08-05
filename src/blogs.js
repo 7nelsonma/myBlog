d1e3b0bebbe291015d77ffc6bbbd7615c7386c79
Mary Nelson <Mary Nelson>
2016-08-05 18:55:17 -0500

    added more blogs

--- src/blogs.js
+++ src/blogs.js
@@ -11,4 +11,16 @@ export const blogs = [
     title: "Mary Third Blog",
     body: "this is my second blog and it is pretty awesome... 😎"
   }
+  {
+    title: "Mary Fourth Blog",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    title: "Mary Fifth Blog",
+    body: "this is my second blog and it is pretty awesome... 😎"
+  },
+  {
+    title: "Mary Sixth Blog",
+    body: "this is my second blog and it is pretty awesome... 😎"
+  }
 ]
