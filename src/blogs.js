5b7bdcea7e8150c5e1046c6fb49f032cf19cd797
Mary Nelson <Mary Nelson>
2016-08-05 19:27:38 -0500

    grammatically correct

--- src/blogs.js
+++ src/blogs.js
@@ -1,38 +1,38 @@
 export const blogs = [
   {
-    title: "Mary First Blog",
+    title: "Mary's First Blog",
     body: "I Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    title: "Mary Second Blog",
+    title: "Mary's Second Blog",
     body: "this is my blog and it is pretty awesome... 😎"
   },
   {
-    title: "Mary Third Blog",
+    title: "Mary's Third Blog",
     body: "this is my blog and it is pretty awesome... 😎"
   }
   {
-    title: "Mary Fourth Blog",
+    title: "Mary's Fourth Blog",
     body: "IV Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    title: "Mary Fifth Blog",
+    title: "Mary's Fifth Blog",
     body: "this is my blog and it is pretty awesome... 😎"
   },
   {
-    title: "Mary Sixth Blog",
+    title: "Mary's Sixth Blog",
     body: "this is my blog and it is pretty awesome... 😎"
   }
   {
-    title: "Mary Seventh Blog",
+    title: "Mary's Seventh Blog",
     body: "VII Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
-    title: "Mary Eighth Blog",
+    title: "Mary's Eighth Blog",
     body: "this is my blog and it is pretty awesome... 😎"
   },
   {
-    title: "Mary Ninth Blog",
+    title: "Mary's Ninth Blog",
     body: "this is my blog and it is pretty awesome... 😎"
   }
 ]
