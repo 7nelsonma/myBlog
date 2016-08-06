900e87f17660ba0c75c35f2d7150859451876995
Mary Nelson <Mary Nelson>
2016-08-05 19:14:42 -0500

    added more content to mary blog

--- src/blogs.js
+++ src/blogs.js
@@ -23,4 +23,16 @@ export const blogs = [
     title: "Mary Sixth Blog",
     body: "this is my second blog and it is pretty awesome... 😎"
   }
+  {
+    title: "Mary Seventh Blog",
+    body: "VII Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {
+    title: "Mary Eighth Blog",
+    body: "this is my second blog and it is pretty awesome... 😎"
+  },
+  {
+    title: "Mary Ninth Blog",
+    body: "this is my second blog and it is pretty awesome... 😎"
+  }
 ]
