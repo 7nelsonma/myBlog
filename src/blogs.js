2c301d11de45bf56f8a98c1e4cc6d49be389be6f
Mary Nelson <Mary Nelson>
2016-08-05 19:18:39 -0500

    deleted and added content to blogs

--- src/blogs.js
+++ src/blogs.js
@@ -1,27 +1,27 @@
 export const blogs = [
   {
     title: "Mary First Blog",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    body: "I Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     title: "Mary Second Blog",
-    body: "this is my second blog and it is pretty awesome... 😎"
+    body: "this is my blog and it is pretty awesome... 😎"
   },
   {
     title: "Mary Third Blog",
-    body: "this is my second blog and it is pretty awesome... 😎"
+    body: "this is my blog and it is pretty awesome... 😎"
   }
   {
     title: "Mary Fourth Blog",
-    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    body: "IV Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     title: "Mary Fifth Blog",
-    body: "this is my second blog and it is pretty awesome... 😎"
+    body: "this is my blog and it is pretty awesome... 😎"
   },
   {
     title: "Mary Sixth Blog",
-    body: "this is my second blog and it is pretty awesome... 😎"
+    body: "this is my blog and it is pretty awesome... 😎"
   }
   {
     title: "Mary Seventh Blog",
@@ -29,10 +29,10 @@ export const blogs = [
   },
   {
     title: "Mary Eighth Blog",
-    body: "this is my second blog and it is pretty awesome... 😎"
+    body: "this is my blog and it is pretty awesome... 😎"
   },
   {
     title: "Mary Ninth Blog",
-    body: "this is my second blog and it is pretty awesome... 😎"
+    body: "this is my blog and it is pretty awesome... 😎"
   }
 ]
